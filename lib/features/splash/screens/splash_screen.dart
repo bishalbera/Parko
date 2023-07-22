0a2e7a8a772799f9380b698ae4c7c3e2e609141f
0Armaan025 <armaan33000@gmail.com>
2023-07-22 13:15:35 +0530

    made splash and home screen without funcationality

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -1,10 +1,32 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:parko/features/home/screens/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key})
+      : super(key: key); // Fix the constructor syntax here.
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return AnimatedSplashScreen(
+      // Add childCurrent argument here to fix the error.
+
+      splash: Text(
+        "Parko",
+        style: GoogleFonts.poppins(
+          color: Colors.green,
+          fontSize: 42,
+          fontWeight: FontWeight.w600,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      backgroundColor: Colors.black12,
+      animationDuration: const Duration(seconds: 3),
+      splashTransition: SplashTransition.slideTransition,
+      pageTransitionType: PageTransitionType.theme,
+      nextScreen: const HomeScreen(),
+    );
   }
 }
