8e94958158065527217a59fcfd89e65a01736cb9
0Armaan025 <armaan33000@gmail.com>
2023-07-23 08:47:54 +0530

    connected with firestore (booking system)

--- lib/features/booking/screens/booking_screen.dart
+++ lib/features/booking/screens/booking_screen.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:parko/common/constants/utils.dart';
 import 'package:parko/features/auth/screens/log%20in/widgets/custom_text_field.dart';
 import 'package:parko/features/models/parking_spot_for_rent.dart';
+import 'package:parko/models/booking.dart';
+
+import '../../../common/constants/constants.dart';
 
 class BookingScreen extends StatefulWidget {
   final ParkingSpotForRent parkingSpot;
@@ -94,21 +98,51 @@ class _BookingScreenState extends State<BookingScreen> {
               height: 20,
             ),
             Center(
-              child: Container(
-                height: 50,
-                width: double.infinity,
-                margin: const EdgeInsets.symmetric(horizontal: 60),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(22),
-                ),
-                alignment: Alignment.center,
-                child: Text(
-                  "Book",
-                  style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+              child: InkWell(
+                onTap: () {
+                  BookingModel model = BookingModel(
+                      bookerUid: ownerId!,
+                      bookerName: _nameController.text,
+                      bookerVehicle: _vehicleController.text,
+                      bookerTime: _timeController.text.toString(),
+                      address: widget.parkingSpot.address,
+                      pricePerHour: widget.parkingSpot.pricePerHour.toString(),
+                      lat: widget.parkingSpot.latitude.toString(),
+                      long: widget.parkingSpot.longitude.toString());
+                  firestore.collection('bookings').add(model.toMap());
+
+                  findLocation(
+                          _nameController.text,
+                          widget.parkingSpot.address,
+                          widget.parkingSpot.latitude,
+                          widget.parkingSpot.longitude)
+                      .then((document) {
+                    if (document != null) {
+                      Map<String, dynamic> newData = {
+                        'isAvailable': false,
+                      };
+                      editLocation(document, newData);
+                    } else {
+                      print("Location not found!");
+                    }
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(horizontal: 60),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(22),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Book",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ),
@@ -117,4 +151,28 @@ class _BookingScreenState extends State<BookingScreen> {
       ),
     );
   }
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> findLocation(
+      String name, String address, double latitude, double longitude) async {
+    final QuerySnapshot<Map<String, dynamic>> querySnapshot =
+        await FirebaseFirestore.instance
+            .collection('locations')
+            .where('name', isEqualTo: name)
+            .where('address', isEqualTo: address)
+            .where('latitude', isEqualTo: latitude)
+            .where('longitude', isEqualTo: longitude)
+            .limit(1)
+            .get();
+
+    return querySnapshot.docs.first;
+  }
+}
+
+void editLocation(DocumentSnapshot<Map<String, dynamic>> document,
+    Map<String, dynamic> newData) {
+  document.reference.update(newData).then((_) {
+    print("Document successfully updated!");
+  }).catchError((error) {
+    print("Error updating document: $error");
+  });
 }
---
+++ lib/models/booking.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class BookingModel {
+  final String bookerUid;
+  final String bookerName;
+  final String bookerVehicle;
+  final String bookerTime;
+  final String address;
+  final String pricePerHour;
+  final String lat;
+  final String long;
+  BookingModel({
+    required this.bookerUid,
+    required this.bookerName,
+    required this.bookerVehicle,
+    required this.bookerTime,
+    required this.address,
+    required this.pricePerHour,
+    required this.lat,
+    required this.long,
+  });
+
+  BookingModel copyWith({
+    String? bookerUid,
+    String? bookerName,
+    String? bookerVehicle,
+    String? bookerTime,
+    String? address,
+    String? pricePerHour,
+    String? lat,
+    String? long,
+  }) {
+    return BookingModel(
+      bookerUid: bookerUid ?? this.bookerUid,
+      bookerName: bookerName ?? this.bookerName,
+      bookerVehicle: bookerVehicle ?? this.bookerVehicle,
+      bookerTime: bookerTime ?? this.bookerTime,
+      address: address ?? this.address,
+      pricePerHour: pricePerHour ?? this.pricePerHour,
+      lat: lat ?? this.lat,
+      long: long ?? this.long,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'bookerUid': bookerUid,
+      'bookerName': bookerName,
+      'bookerVehicle': bookerVehicle,
+      'bookerTime': bookerTime,
+      'address': address,
+      'pricePerHour': pricePerHour,
+      'lat': lat,
+      'long': long,
+    };
+  }
+
+  factory BookingModel.fromMap(Map<String, dynamic> map) {
+    return BookingModel(
+      bookerUid: map['bookerUid'] as String,
+      bookerName: map['bookerName'] as String,
+      bookerVehicle: map['bookerVehicle'] as String,
+      bookerTime: map['bookerTime'] as String,
+      address: map['address'] as String,
+      pricePerHour: map['pricePerHour'] as String,
+      lat: map['lat'] as String,
+      long: map['long'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory BookingModel.fromJson(String source) =>
+      BookingModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'BookingModel(bookerUid: $bookerUid, bookerName: $bookerName, bookerVehicle: $bookerVehicle, bookerTime: $bookerTime, address: $address, pricePerHour: $pricePerHour, lat: $lat, long: $long)';
+  }
+
+  @override
+  bool operator ==(covariant BookingModel other) {
+    if (identical(this, other)) return true;
+
+    return other.bookerUid == bookerUid &&
+        other.bookerName == bookerName &&
+        other.bookerVehicle == bookerVehicle &&
+        other.bookerTime == bookerTime &&
+        other.address == address &&
+        other.pricePerHour == pricePerHour &&
+        other.lat == lat &&
+        other.long == long;
+  }
+
+  @override
+  int get hashCode {
+    return bookerUid.hashCode ^
+        bookerName.hashCode ^
+        bookerVehicle.hashCode ^
+        bookerTime.hashCode ^
+        address.hashCode ^
+        pricePerHour.hashCode ^
+        lat.hashCode ^
+        long.hashCode;
+  }
+}
